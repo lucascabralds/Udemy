fbfca3a5b92afb39fd3a03bf5f1202e223130237
lucascabralds <lucascabraldossantos53@gmail.com>
2022-02-01 00:03:37 -0300

    Em nossa segunda aula aprendemos o conceito de strings dentro de um print, aprendemos que se caso nos queiramos realizar uma quebra de linha em python podemos usar o conceito de /n e se caso queiramos mostra alguma sintaxe sem executar podemos inserir o r antes da abertura de aspas.

---
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/1 - Aula 01 - Print/Aula02Strings.py
@@ -0,0 +1,12 @@
+"""
+str- string
+"""
+
+print("Essa é uma 'string' (str).") # o sistema de print no python ele sempre interliga como ela foi aberta,logo se ela abriu com aspas simples ela vai fechar com a proxima aspas simples
+print('Essa é uma "string" (str).')  #A mesma se aplica com aspas duplas
+print('Essa é uma  \'string\' (str).') #Caso queira apresentar algum valor, alguma string pode ser usado a barra e invertida, mas não é recomendado pois o codigo fica confuso.
+print("Essa é uma  \"string\" (str).")
+print('Esse é meu \n (str.')  #A sintaxe /n e usada em uma quebra de linha.
+print(r'Esse é meu \n (str.')  #caso queira apresenta alguma sintaxe e nao quer que essa linha não seja executado nada, use a palavra r antes das aspas.
+
+print('Essa é uma "string" (str).')  #Caso queira apresenta alguma string e recomendado a abrir o print com simples e apresenta alguma sintaxe com aspas duplas.
