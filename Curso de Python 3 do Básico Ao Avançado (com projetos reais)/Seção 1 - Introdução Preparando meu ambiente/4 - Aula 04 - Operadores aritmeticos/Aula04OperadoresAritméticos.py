be0b9f74e7b4a5a08533d0874662ed4e044e0671
lucascabralds <lucascabraldossantos53@gmail.com>
2022-02-07 23:02:28 -0300

    Em nossa quarta aula aprendemos a usar a maioria dos operadores aritmeticos, e aplicamos isso com dados é também aprendemos a usar esses operadores aritmeticos com Polimorfismo.

---
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/4 - Aula 04 - Operadores aritmeticos/Aula04OperadoresAritméticos.py
@@ -0,0 +1,19 @@
+"""
+Operações aritmeticas
++,-,*,/,//,**,%,()
+"""
+
+print('Multiplicação * ', 10*10)
+print('Adição + ',10+10)
+print('Subtração - ', 10-5)
+print('Divisão / ',10/2)
+print('Divisão inteira // ',10.5//3)
+print('Potenciação **', 2**3)
+print('Operador de resto % ', 10%3)
+
+#Atos de polifomismo
+
+print(20*"A")
+print(20*2)
+print('20'+'A')
+print(20+2)
