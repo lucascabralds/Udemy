fcbccaa4ae6bdf80b2a0162ff4e82a82dac85c07
lucascabralds <lucascabraldossantos53@gmail.com>
2022-02-07 22:28:20 -0300

    Em nossa primeira aula de Python aprendemos o conceito de print,das f…

--- Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/1 - Aula 01 - Print/Aula01Print.py
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/1 - Aula 01 - Print/Aula01Print.py
@@ -2,12 +2,10 @@ print(123456)  #Quando se apresenta so valor inteiro não precisa de aspas simpl
 print('Luiz', 'Otávio', 'Outra coisa')  #E possivel printar separado por ","
 print('Luiz', 'Otávio', sep='-')  #"A sintaxe sep si consistem em separar por valor com aquilo que você determina,nesse caso estamos usando o '-'
 print('João', 'e', 'Maria', sep='-')  # A sintaxe end si consistem qual valor vai ter no final daquele print
-
 #Outro ponto importante, o print separado por virgula e obrigado apos a virgula ter um espaço
 
 """
 Exercicio abaixo de apresenta um cpf aleatorio
 """
-
 print(123, 456, 789, sep=".", end="-")
 print(10)
