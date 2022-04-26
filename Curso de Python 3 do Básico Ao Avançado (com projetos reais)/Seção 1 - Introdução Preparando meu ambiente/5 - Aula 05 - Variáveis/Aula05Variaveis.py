bc29bb3feb5e6094bce8444988902718b36dfb7a
lucascabralds <lucascabraldossantos53@gmail.com>
2022-04-25 22:52:29 -0300

    Em nossa quinta aula aprendemos o conceito de declaração de variaveis, sendo entre elas, Int,Float é Bool

---
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/5 - Aula 05 - Variáveis/Aula05Variaveis.py
@@ -0,0 +1,10 @@
+"""
+Iniciar com letra, pode conter números, seprarar, letras minúsculas
+"""
+
+nome = 'Luiz Otávio'
+idade = 32 #int
+altura = 1.80 #float
+e_maior= idade > 18 #bool
+peso = 80
+imc = 'temp'
