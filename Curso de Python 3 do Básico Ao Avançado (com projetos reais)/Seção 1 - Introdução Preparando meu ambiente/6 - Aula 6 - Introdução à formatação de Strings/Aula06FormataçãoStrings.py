1eccb753fdb862f69d19338f8c753a3145a6fd4f
lucascabralds <lucascabraldossantos53@gmail.com>
2022-05-04 20:37:41 -0300

    qual aula e

---
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/6 - Aula 6 - Introdução à formatação de Strings/Aula06FormataçãoStrings.py
@@ -0,0 +1,10 @@
+nome = "Luiz Otávio"
+idade = 32 #int
+altura = 1.80 #float
+e_maior = idade > 18 #bool
+peso = 80
+imc = peso / (altura **2)
+
+print(nome, 'tem', idade, ' anos de idade e seu imc é',imc)
+print(f'{nome} tem {idade} anos de idade e seu imc é {imc:.2f}')
+print('{im} {n} tem {i} anos e seu imc é '.format(n=nome, i=idade,im=imc))
