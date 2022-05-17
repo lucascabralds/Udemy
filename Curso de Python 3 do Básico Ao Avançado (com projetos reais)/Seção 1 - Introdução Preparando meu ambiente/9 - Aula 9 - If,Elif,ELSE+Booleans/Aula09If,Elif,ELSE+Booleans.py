9e7eeb776a3df73675fdd92e48b6807550039b26
lucascabralds <lucascabraldossantos53@gmail.com>
2022-05-16 22:13:15 -0300

    Em nossa nona aula aprendemos o conceito de sistemas de condições usando a sintaxe IF,ELIF é também ELSE

---
+++ Curso de Python 3 do Básico Ao Avançado (com projetos reais)/Seção 1 - Introdução Preparando meu ambiente/9 - Aula 9 - If,Elif,ELSE+Booleans/Aula09If,Elif,ELSE+Booleans.py
@@ -0,0 +1,19 @@
+"""
+Condições IF, ELIF e ELSE - Aula 4
+"""
+
+if False:
+    print('Verdadeiro')
+    print('Teste teste 2')
+
+elif True:
+    print("Agora e verdadeiro")
+    nome=input("Qual o seu nome?")
+    print(f'Seu noe é {nome}')
+
+elif False:
+    print('Mais uma verdadeira')
+    print(22+22)
+else:
+    print("não é verdadeiro")
+    print("Olá")
